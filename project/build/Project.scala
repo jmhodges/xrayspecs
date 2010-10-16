eb734fce9a3f9cbfe41261bf11f4c851d45dc88e
Robey Pointer <robey@twitter.com>
2010-10-15 18:00:45 -0700

    update so it can build for scala 2.7.7 or 2.8.0.

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,14 +2,20 @@ import sbt._
 
 import com.twitter.sbt._
 
-class Project(info: ProjectInfo) extends StandardProject(info) {
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5"
-  val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
-  val twitterJson = "com.twitter" %% "json" % "2.1.4"
-  val configgy = "net.lag" % "configgy" % "2.0.0"
+class Project(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
+  val specs = buildScalaVersion match {
+    case "2.7.7" => "org.scala-tools.testing" % "specs" % "1.6.2.1"
+    case _ => "org.scala-tools.testing" %% "specs" % "1.6.5"
+  }
+  val json = buildScalaVersion match {
+    case "2.7.7" => "com.twitter" % "json" % "1.1.7"
+    case _ =>  "com.twitter" %% "json" % "2.1.4"
+  }
+  val configgy = buildScalaVersion match {
+    case "2.7.7" => "net.lag" % "configgy" % "1.6.4"
+    case _ => "net.lag" % "configgy" % "2.0.0"
+  }
 
   override def disableCrossPaths = false
-
-  Credentials(Path.userHome / ".ivy2" / "credentials", log)
-  val publishTo = "nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+  override def subversionRepository = Some("http://svn.local.twitter.com/maven-public/")
 }
