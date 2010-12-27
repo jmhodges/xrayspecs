bd9df334c5847fbd46926cc1aaf8b90c60fcad4d
Jeremy Cloud <jeremy.cloud@me.com>
2010-12-27 12:26:49 -0500

    update to configgy 2.0.2

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -5,15 +5,11 @@ import com.twitter.sbt._
 class Project(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
   val specs = buildScalaVersion match {
     case "2.7.7" => "org.scala-tools.testing" % "specs" % "1.6.2.1"
-    case _ => "org.scala-tools.testing" %% "specs" % "1.6.5"
-  }
-  val json = buildScalaVersion match {
-    case "2.7.7" => "com.twitter" % "json" % "1.1.7"
-    case _ =>  "com.twitter" %% "json" % "2.1.4"
+    case _ => "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6"
   }
   val configgy = buildScalaVersion match {
     case "2.7.7" => "net.lag" % "configgy" % "1.6.4"
-    case _ => "net.lag" % "configgy" % "2.0.0"
+    case _ => "net.lag" % "configgy" % "2.0.2"
   }
 
   override def disableCrossPaths = false
